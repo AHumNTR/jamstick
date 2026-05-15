4886e48152777d9a96f6ff31d6efc9891c24f3fa
sloime <tunaozdemir01@gmail.com>
2026-05-16 01:35:14 +0300

    added item and movement

--- item.gd
+++ item.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+class_name moveable
 var animation_speed = 2
 var moving = false
 var tile_size = 128
@@ -9,6 +9,7 @@ var inputs = {
 	"up": Vector2.UP,
 	"down": Vector2.DOWN
 }
+var tag = "item1"
 
 @onready var ray = $RayCast2D
 
@@ -16,14 +17,6 @@ func _ready():
 	#position = position.snapped(Vector2.ONE * tile_size)
 	#position += Vector2.ONE * tile_size / 2
 	pass
-
-func _unhandled_input(event):
-	if moving:
-		return
-	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
-			move(dir)
-			
 func move(dir):
 	ray.target_position = inputs[dir] * tile_size
 	ray.force_raycast_update()
@@ -36,5 +29,8 @@ func move(dir):
 		await tween.finished
 		moving = false
 		
-	elif ray.get_collider() is moveable:
-		ray.get_collider().move(dir)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	#if area is moveable
+	pass
