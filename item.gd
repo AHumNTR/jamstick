8ad306a49df95950d2ccb83a13a7a56a7ff81f48
HumN <ahumantr@gmail.com>
2026-05-16 14:27:58 +0300

    tilemapgenerator

--- item.gd
+++ item.gd
@@ -3,12 +3,7 @@ class_name moveable
 var animation_speed = 2
 var moving = false
 var tile_size = 128
-var inputs = {
-	"right": Vector2.RIGHT,
-	"left": Vector2.LEFT,
-	"up": Vector2.UP,
-	"down": Vector2.DOWN
-}
+var typeIndex=-1
 var tag = "item1"
 
 @onready var ray = $RayCast2D
@@ -28,9 +23,22 @@ func move(dir):
 		#$AnimationPlayer.play(dir)
 		await tween.finished
 		moving = false
-		
+	elif ray.get_collider() is moveable :
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position + dir * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		moving = true
+		#$AnimationPlayer.play(dir)
+		await tween.finished
+		var other=ray.get_collider() as moveable
+		if(other.typeIndex==typeIndex):
+			other.queue_free()
+			queue_free()
+
 
 
 func _on_area_entered(area: Area2D) -> void:
-	#if area is moveable
+	if(area is moveable):
+		if(area.typeIndex==typeIndex):
+			area.queue_free()
+			queue_free()
 	pass
---
+++ scripts/tilemap_spawner.gd
@@ -0,0 +1,20 @@
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+@export var items: Array[PackedScene]
+func _ready() -> void:
+	for cell in get_used_cells():
+		var source_id = get_cell_source_id(cell)
+		if(source_id>1):
+			var atlas_coords = get_cell_atlas_coords(cell)
+			var item= items[source_id-2].instantiate() as moveable
+			add_child(item)
+			item.position=map_to_local(cell)
+			item.typeIndex=source_id
+			var tileset_source: TileSetAtlasSource = tile_set.get_source(source_id) as TileSetAtlasSource
+			var sprite=(item.get_node("Sprite2D") as Sprite2D)
+			sprite.texture=tileset_source.texture
+			sprite.region_enabled = true
+			sprite.region_rect = tileset_source.get_tile_texture_region(atlas_coords)
+			set_cell(cell,1,Vector2i(0,0))
